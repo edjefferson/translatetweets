9ba4be5aa02ed8fcb60b1ae378edba6bcbcec0f6
edjefferson <edjefferson@reevoo.com>
2013-02-03 14:32:14 +0000

    record last 50 ids to be safe

--- translatetweets.rb
+++ translatetweets.rb
@@ -32,10 +32,10 @@ end
   LatestTweet = LeTwitter.search("from:#{ENV['TWITTERHANDLE']}", :count => 25, :result_type => "recent").results.reverse.each do |status|
     conn = PGconn.connect(ENV['DB_ADDRESS'], ENV['DB_PORT'], '', '', ENV['DB_NAME'], ENV['DB_USER'], ENV['DB_PASSWORD'])
     readout = conn.query("SELECT * from tweets").values.to_a
-    @last25 = Array.new
+    @last50 = Array.new
     readout.each_with_index do |x , y|
       
-      @last25 << x[0]
+      @last50 << x[0]
     end  
     
 
@@ -43,7 +43,7 @@ end
     tweettext=status.text
     sid=tweetid.to_s
     
-    if !@last25.include?sid
+    if !@last50.include?sid
       
       translatedtweet = translator.translate(tweettext,"en","fr","text/html")
       puts translatedtweet
@@ -62,7 +62,7 @@ end
       finaltweet=splittweet.join(" ") 
       puts finaltweet.length
       puts finaltweet[0..139].length 
-      if finaltweet.length<141
+      if finaltweet.length<=140
         puts finaltweet
         Twitter.update(finaltweet)
       else  
@@ -72,7 +72,7 @@ end
       end
       
 
-      if conn.query("select  count(id) from tweets;").values.to_a[0][0].to_i>25
+      if conn.query("select  count(id) from tweets;").values.to_a[0][0].to_i>=50
         conn.query("DELETE FROM tweets using (select min(id) from tweets) r where id=r.min;")
       end
       
